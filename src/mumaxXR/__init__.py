07e13043730fd30a78f5ece853620e5db6a05dad
timvgl <97906175+timvgl@users.noreply.github.com>
2025-03-04 15:55:30 +0100

    Update __init__.py

--- src/mumaxXR/__init__.py
+++ src/mumaxXR/__init__.py
@@ -1,3 +1,3 @@
 #Created by Tim Vogel
 from mumaxXR.OvfEngine import OvfEngine, convert_TableFile_to_dataset
-from mumaxXR.scriptParser import process_command
+from mumaxXR.scriptParser import process_command, get_globalenv
